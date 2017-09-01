f4a0cec40af2caebaeb3f51afa191baee3f1ec6d
zhanfeng <18272734568@163.com>
2017-09-01 14:24:12 +0800

    Initial Commit

---
+++ C++Test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++Test
+//
+//  Created by zhanfeng on 2017/9/1.
+//  Copyright © 2017年 lzf. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
